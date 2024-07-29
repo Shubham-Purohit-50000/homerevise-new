6b0e867a8baa039fe1356b7ee5f18477cda91be0
Shubham Local <Shubhampurohit10jan1997@gmail.com>
2024-07-29 21:49:23 +0530

    added laravel comment in quiz edit blade

--- resources/views/quiz/edit.blade.php
+++ resources/views/quiz/edit.blade.php
@@ -175,7 +175,8 @@
                                 </div>
                                 
                                 
-                                <div class="form-group d-none">
+                               {{-- 
+                                <div class="form-group">
                                     <label for="total_quiz_time">Add to Course</label>
                                     <select class="js-example-basic-multiple form-control" name="courses[]" multiple="multiple" >
                                         @if($quiz->course_id != "null") 
@@ -192,6 +193,7 @@
                                         @endif
                                     </select>
                                 </div>
+                                --}}
 
                                 <div class="form-group">
                                     <label for="scheduler">Schedule Quiz</label>
